d9e3242649c7ef1c8ad0c8a4dcf8f4b708736a7b
Randy Au <randy@meetup.com>
2013-02-06 14:48:52 -0500

    Voosh

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-excel_chart_counter
+excel chart counter
 ===================
+
+This is a very dumb one-off script that crawls throughs a list of directories, searches for excel files (specifically xlsx, but should handle xls fine w/ some tweaks). Then it goes through all the available worksheets and records the count of chart objects in the file.
+
+Input assumptions
+===
+
+IMPORTANT: This script assumes all files follow a naming convention of having YYYY-MM-DD\_filename.xlsx and it uses that as a the file's creation date. It does NOT read the file create/modify times.
+
+It'll group all the data on a daily basis, going by the file name date string.
+
+Output 
+===
+It'll spit the results of the count into results.txt, tab-deliminated, ready for dumping into excel to chart your chart of charts
+
+Disclaimer
+===
+This is a toy script hacked together for fun and provided as-is. Use at your own risk. I'm not responsible if it destroys your data.
+
--- chart_counter.py
+++ chart_counter.py
@@ -8,73 +8,80 @@ import datetime
 import dateutil
 import dateutil.parser
 
-xl = win32com.client.Dispatch("Excel.Application")
 
-#Turn off Excel's updating stuff so it doesn't flicker and waste tiem rendering
-xl.Application.DisplayAlerts = False
-xl.Application.ScreenUpdating = False
-xl.Application.EnableEvents = False
-xl.Application.AskToUpdateLinks = False 
+def do_everything():
 
-dir_queue = ["C:\\Somedirectory\\with\\your"
-"D:\\some\\other\\directory\\no\\trailing\\slashes\\pls"
-]
+  xl = win32com.client.Dispatch("Excel.Application")
 
-YEAR_INTERESTED = '2012'
+  #Turn off Excel's updating stuff so it doesn't flicker and waste tiem rendering
+  xl.Application.DisplayAlerts = False
+  xl.Application.ScreenUpdating = False
+  xl.Application.EnableEvents = False
+  xl.Application.AskToUpdateLinks = False 
 
-files_processed = []
+  dir_queue = ["C:\\Somedirectory\\with\\your"
+  "D:\\some\\other\\directory\\no\\trailing\\slashes\\pls"
+  ]
 
-final_filelist = []
-#regex to grab dates for grouping
-file_re =  re.compile("(\d{4}-\d{1,2}-\d{1,2})[-_].+\.xlsx")
-for doc_root in dir_queue:
-  filelist = os.listdir(doc_root)
-  filelist = [x for x in filelist if "xlsx" in x]
-  
-  filelist = [x for x in filelist if file_re.search(x)] #only valid filenames past this point
-  filelist2 = []
-  for x in filelist:
-    try: #skip any files that don't have YEAR_INTERESTED in the name
-      if YEAR_INTERESTED in file_re.search(x).groups()[0]:
-        filelist2.append(x)
-    except:
-      pass 
-  final_filelist.append([doc_root, filelist2])
+  YEAR_INTERESTED = '2012'
 
-chart_history = {}
+  files_processed = []
 
-#now iterate through the whole of files
-for doc_root, filelist in final_filelist:
-  for fn in filelist:
-    datekey = dateutil.parser.parse( file_re.search(fn).groups()[0] )
-    charts = 0
-    try:
-      files_processed.append(doc_root+"\\"+fn)
-      xl.WorkBooks.Open( doc_root+"\\"+fn )
-      ws_len = len(xl.WorkBooks(fn).WorkSheets)
-      for i in xrange(ws_len):
-        #i+1 is 0indexed -> 1indexed
-        charts += len(xl.WorkBooks(fn).WorkSheets(i+1).ChartObjects())
-      xl.WorkBooks(fn).Close(SaveChanges=False)
-    except:
-      print fn,"failed"
-    chart_history[datekey] = chart_history.setdefault(datekey,0)+charts
- 
-#List of files that were processed
-dfile = open("debug.txt",'w')
-dfile.write("\n".join(files_processed))
-dfile.close()
+  final_filelist = []
+  #regex to grab dates for grouping
+  file_re =  re.compile("(\d{4}-\d{1,2}-\d{1,2})[-_].+\.xlsx")
+  for doc_root in dir_queue:
+    filelist = os.listdir(doc_root)
+    filelist = [x for x in filelist if "xlsx" in x]
+    
+    filelist = [x for x in filelist if file_re.search(x)] #only valid filenames past this point
+    filelist2 = []
+    for x in filelist:
+      try: #skip any files that don't have YEAR_INTERESTED in the name
+        if YEAR_INTERESTED in file_re.search(x).groups()[0]:
+          filelist2.append(x)
+      except:
+        pass 
+    final_filelist.append([doc_root, filelist2])
 
-#simple results output, tab separated
-ofile = open("results.txt",'w')
-for k,v in chart_history.items():
-  ofile.write( k.date().isoformat() )
-  ofile.write("\t")
-  ofile.write( str(v) )
-  ofile.write("\n")
+  chart_history = {}
 
-#Reactivate all the updating stuff in excel, but it's still a bit sluggish after, might be easier to close the program if you want to actually use it
-xl.Application.DisplayAlerts = True
-xl.Application.ScreenUpdating = True
-xl.Application.EnableEvents = True
-xl.Application.AskToUpdateLinks = True
+  #now iterate through the whole of files
+  for doc_root, filelist in final_filelist:
+    for fn in filelist:
+      datekey = dateutil.parser.parse( file_re.search(fn).groups()[0] )
+      charts = 0
+      try:
+        files_processed.append(doc_root+"\\"+fn)
+        xl.WorkBooks.Open( doc_root+"\\"+fn )
+        ws_len = len(xl.WorkBooks(fn).WorkSheets)
+        for i in xrange(ws_len):
+          #i+1 is 0indexed -> 1indexed
+          charts += len(xl.WorkBooks(fn).WorkSheets(i+1).ChartObjects())
+        xl.WorkBooks(fn).Close(SaveChanges=False)
+      except:
+        print fn,"failed"
+      chart_history[datekey] = chart_history.setdefault(datekey,0)+charts
+   
+  #List of files that were processed
+  dfile = open("debug.txt",'w')
+  dfile.write("\n".join(files_processed))
+  dfile.close()
+
+  #simple results output, tab separated
+  ofile = open("results.txt",'w')
+  for k,v in chart_history.items():
+    ofile.write( k.date().isoformat() )
+    ofile.write("\t")
+    ofile.write( str(v) )
+    ofile.write("\n")
+
+  #Reactivate all the updating stuff in excel, but it's still a bit sluggish after, might be easier to close the program if you want to actually use it
+  xl.Application.DisplayAlerts = True
+  xl.Application.ScreenUpdating = True
+  xl.Application.EnableEvents = True
+  xl.Application.AskToUpdateLinks = True
+
+if __name__ == "__main__":
+  print "Counting! Your excel will be locked up for a while"
+  do_everything()
